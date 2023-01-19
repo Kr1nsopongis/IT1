5f2f9bca24fb4377b3ddead823707248f37e8823
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-01-19 18:53:21 +0100

    d
    dd

---
+++ 3f/script.js
@@ -0,0 +1,18 @@
+let bil = [
+    { bilmerke: "Volkswagen", forkortelse: "VW" },
+    { bilmerke: "Toyota", forkortelse: "tyota" },
+    { bilmerke: "Ford", forkortelse: "frd" }
+  ];
+
+//   function sammenliknFunksjon(a, b) {
+//     if (a > b) {
+//       return 1;
+//     } else if (a < b) {
+//       return -1;
+//     } else {
+//       return 0;
+//     }
+//   }
+
+console.log("Forkortelsen til ", bil.bilmerke," er ", bil.forkortelse)
+// console.log(bil)
