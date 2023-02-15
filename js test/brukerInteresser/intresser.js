2f58e900d40de3c2b7a3dff70479280005ba1f6e
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-02-15 08:31:41 +0100

    Firebase intro
    Tok legg til ut av formen

--- js test/brukerInteresser/intresser.js
+++ js test/brukerInteresser/intresser.js
@@ -1,14 +1,17 @@
+document.getElementById("leggTil").addEventListener("click", (evt) => {
 
-
-
-document.getElementById("leggTil").addEventListener("submit", (evt) => {
+    if (document.getElementById("interesser").value = "") {
+        console.log("LEGG TIL INTERESSE")
+    } else {
+        var a = document.getElementById("interesser");
+        var candidate = document.getElementById("input");
+        var li = document.createElement("li");
+        li.setAttribute('id', candidate.value);
+        li.appendChild(document.createTextNode(candidate.value));
+        a.appendChild(li);
+    }
     
-    var a = document.getElementById("interesser");
-    var candidate = document.getElementById("input");
-    var li = document.createElement("li");
-    li.setAttribute('id', candidate.value);
-    li.appendChild(document.createTextNode(candidate.value));
-    a.appendChild(li);
+    
 })
 
 document.getElementById("fjern").addEventListener("click", (evt) =>{
