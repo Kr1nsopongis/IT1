310e9d769b696bfeceb5222b7f58400874746cd2
evstellberg <112948429+evstellberg@users.noreply.github.com>
2023-04-04 13:04:22 +0200

    fikset feil
    Fikset slik at bildeopplasting faktisk funker

--- js test/brukerSide/brukerSide.js
+++ js test/brukerSide/brukerSide.js
@@ -2,5 +2,5 @@ document.getElementById("submit").addEventListener("click", submit);
 
 function submit() {
     let profilBilde = document.getElementById("profilBildeUpload").files[0];
-    document.getElementById("profilBilde").src = URL.createObjectURL(profilBilde);
+    document.getElementById("profilBilde").style.background = "url(" + URL.createObjectURL(profilBilde) + ")";
 }
