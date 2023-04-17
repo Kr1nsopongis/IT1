cba2c532821545daf2a37a01659e67613e710cce
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-04-17 19:04:57 +0200

    Legge til funksjon hvor man kan legge til dagens bilde har fulgt giden til mdn
    https://developer.mozilla.org/en-US/docs/Web/API/Media_Capture_and_Streams_API/Taking_still_photos

--- js test/brukerSide/brukerSide.js
+++ js test/brukerSide/brukerSide.js
@@ -1,7 +1,85 @@
-document.getElementById("submit").addEventListener("click", submit);
+const width = 150;
+const height = 100;
+
+let streaming = false;
+let canvas = null;
+let bilde = null;
+let startKnapp = null;
+let buttonClicked = false;
+
+    video = document.getElementById("video");
+    canvas = document.getElementById("canvas");
+    bilde = document.getElementById("bilde");
+    startKnapp = document.getElementById("startKnapp");
+
+navigator.mediaDevices 
+    .getUserMedia({ video: true, audio: false })
+    .then((stream) => {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(`An error occurred: ${err}`);
+      });
+
+    video.addEventListener(
+      "canplay",
+      (ev) => {
+        if (!streaming) {
+          height = video.videoHeight / (video.videoWidth / width);
+
+          if (isNaN(height)) {
+            height = width / (4 / 3);
+          }
+
+          video.setAttribute("width", width);
+          video.setAttribute("height", height);
+          
+          streaming = true;
+        }
+      },
+      false
+    );
+    
+    startKnapp.addEventListener(
+      "click",
+      (ev) => {
+        buttonClicked = true;
+        takepicture();
+        ev.preventDefault();
+      },
+      false
+    );
+
+    function takepicture() {
+        if (buttonClicked){
+            const context = canvas.getContext("2d");
+        if (width && height) {
+          canvas.width = width;
+          canvas.height = height;
+          context.drawImage(video, 0, 0, width, height);
+    
+          const data = canvas.toDataURL("image/png");
+        //   bilde.setAttribute("src", data);
+        } else {
+          clearphoto();
+        }
+        }
+    }    
+
+    function clearphoto() {
+        const context = canvas.getContext("2d");
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+    
+        const data = canvas.toDataURL("image/png");
+        bilde.setAttribute("src", data);
+      }
+
+document.getElementById("startKnapp").addEventListener("click", submit);
 
 function submit() {
-    let profilBilde = document.getElementById("profilBildeUpload").files[0];
+    let profilBilde = data
     document.getElementById("profilBilde").style.background = "url(" + URL.createObjectURL(profilBilde) + ")";
 }
 
@@ -10,7 +88,6 @@ console.log(brukerData);
 let splittetNavn = brukerData[0].navn.split(" ");
 
 document.getElementById("brukernavn").value = brukerData[0].brukernavn;
-document.getElementById("passord").value = brukerData[0].passord;
 document.getElementById("email").value = brukerData[0].email;
 document.getElementById("fornavn").value = splittetNavn[0];
 document.getElementById("etternavn").value = splittetNavn[1];
