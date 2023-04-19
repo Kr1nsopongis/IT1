dd48aba90277bbab04931a68a84a4dc8ba2aa477
evstellberg <112948429+evstellberg@users.noreply.github.com>
2023-04-19 15:17:19 +0200

    lagt til pointer på intresser

--- js test/startside/start.js
+++ js test/startside/start.js
@@ -1,5 +1,6 @@
 document.getElementById("nesteSide").addEventListener('click',sendVidere);
 document.getElementById("logInn").addEventListener("click", sendTilLoginn)
+document.getElementById("sand").addEventListener("click", sand)
 
 function sendVidere() {
     document.location.href = "../brukerRegistrering/registrering.html";
@@ -9,4 +10,6 @@ function sendTilLoginn() {
     document.location.href = "../loginnside/logg-inn.html"
 }
 
-
+function sand() {
+    document.location.href = "https://www.youtube.com/watch?v=2tLf1JO5bvE"
+}
