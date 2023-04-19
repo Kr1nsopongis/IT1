a40cf7e6a485d16b3c5323469cabb5e863c93737
evstellberg <112948429+evstellberg@users.noreply.github.com>
2023-04-19 14:50:49 +0200

    oppdaterte knapper startside
    - flyttet og stylet registreringsknapp
    - la til logg inn knapp

--- js test/startside/start.js
+++ js test/startside/start.js
@@ -1,5 +1,12 @@
-function sendVidere(){
-    document.location.href = "../brukerRegistrering/registrering.html"
+document.getElementById("nesteSide").addEventListener('click',sendVidere);
+document.getElementById("logInn").addEventListener("click", sendTilLoginn)
+
+function sendVidere() {
+    document.location.href = "../brukerRegistrering/registrering.html";
 }
 
-document.getElementById("nesteSide").addEventListener('click',sendVidere)
+function sendTilLoginn() {
+    document.location.href = "../loginnside/logg-inn.html"
+}
+
+
