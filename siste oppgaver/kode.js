d590e957c6478afe365efbc4b6f37964f3b7c0ba
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-05-30 18:12:19 +0200

    Update kode.js

--- siste oppgaver/kode.js
+++ siste oppgaver/kode.js
@@ -1,27 +1,34 @@
-alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","æ","ø","å","*"]
+alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","*"]
 
-let setning = "hei jeg heter"
+let setning = "jeg heter dette"
 let nyttOrd = ""
-let forskyvning = 2
+let forskyvning = 3
+
+if (splittetSetning[i] == " "){
+    splittetSetning[i] = "*"
+    console.log(splittetSetning)
+}
 
 function kryptering() {
     splittetSetning = setning.split("");
 
     for(let i = 0; i < splittetSetning.length; i++ ){
-        if (splittetSetning[i] == " "){
-            splittetSetning[i] = "*"
-            console.log(splittetSetning)
-        }
-            
+    
         
-        if (alfabet.indexOf(splittetSetning[i] < (alfabet.length - forskyvning))){
-            nyBokstav
+        console.log(alfabet.indexOf(splittetSetning[i]))
+        
+        if (alfabet.indexOf(splittetSetning[i]) > (alfabet.length - forskyvning)){
+            let nyBokstav = splittetSetning[splittetSetning[i]+forskyvning - alfabet.length]
+            nyttOrd = nyttOrd +nyBokstav
+            console.log(nyBokstav)
+        } else {
+            nyBokstav = alfabet[alfabet.indexOf(splittetSetning[i] + forskyvning)] 
+            nyttOrd = nyttOrd + nyBokstav
+            };
         }
-            
-        ;
-        let nyBokstav = alfabet[alfabet.indexOf(splittetSetning[i + forskyvning])] 
-        nyttOrd = nyttOrd + nyBokstav
-        }
+        
+        
+       
     
     console.log(nyttOrd)
 }
