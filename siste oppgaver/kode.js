7ed370776bf036c3ea896f1f2f174ae5e5c1adf8
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-05-31 19:38:56 +0200

    Update kode.js

--- siste oppgaver/kode.js
+++ siste oppgaver/kode.js
@@ -1,13 +1,16 @@
-alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","*"]
+let alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","*"]
+let ordListe = ["og","i","det","på","som","er","en","til","å","han","av","for","med","at","var","de","ikke","den","har","jeg","om","et","men","så","seg","hun","hadde","fra","vi","du","kan","da","ble","ut","skal","vil","ham","etter","over","ved","også","bare","eller","sa","nå","dette","noe","være","meg","mot","opp","der","når","inn","dem","kunne","andre","blir","alle","noen","sin","ha","år","henne","må","selv","sier","få","kom","denne","enn","to","hans","bli","ville","før","vært","skulle","går","her","slik","gikk","mer","hva","igjen","igjen","fikk","man","alt","mange","dash","ingen","dash","ingen","får","oss","under","siden","hele","dag","gang","sammen"]
 
-let setning = "krinsopongis"
-let nyttOrd = ""
-let forskyvning = 3
+let setning = "";
+let nyttOrd = "";
+let forskyvning = 3;
+let ord = []
 
+function definere(){
+    setning = document.getElementById("kryptering").value;
+}
 
 function kryptering() {
-
-    console.log(alfabet)
     
     let splittetSetning = setning.split("");
 
@@ -19,22 +22,37 @@ function kryptering() {
         console.log(splittetSetning)
     }
 
-        console.log(alfabet.indexOf(splittetSetning[i]))
+        // console.log(alfabet.indexOf(splittetSetning[i]))
         
-        if (alfabet.indexOf(splittetSetning[i]) > (alfabet.length - forskyvning)){
+        if (alfabet.indexOf(splittetSetning[i]) > (alfabet.length - forskyvning -1)){
             let nyBokstav  = alfabet[alfabet.indexOf(splittetSetning[i]) + forskyvning - alfabet.length]
             nyttOrd = nyttOrd +nyBokstav
-            console.log(nyBokstav)
         } else {
             nyBokstav = alfabet[alfabet.indexOf(splittetSetning[i]) + forskyvning] 
             nyttOrd = nyttOrd + nyBokstav 
             };
         }
+      
+}
+
+
+function dekrypter() {
+    setning = document.getElementById("dekryptering").value
+
+    for(let i = 0; i < 27; i++){
+        forskyvning = i
+        kryptering()
+        ord.push(nyttOrd)
         console.log(nyttOrd)
-        
+        nyttOrd = ""
+    }
+}
+
+function startKryptering(){
+    definere()
+    kryptering()
     console.log(nyttOrd)
 }
 
-document.getElementById("knapp").addEventListener('click',kryptering)
+document.getElementById("knapp").addEventListener('click', dekrypter)  
 
-<
