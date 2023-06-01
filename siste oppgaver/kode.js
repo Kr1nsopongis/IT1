1bd171d23bce1421bf7c4adba9b9419e9e3f40e8
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-06-01 14:14:29 +0200

    Prøver å få bruteforcen til å funke men har vondt i hodet av å tenke for mye så må ha en pause

--- siste oppgaver/kode.js
+++ siste oppgaver/kode.js
@@ -2,9 +2,12 @@ let alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","
 let ordListe = ["og","i","det","på","som","er","en","til","å","han","av","for","med","at","var","de","ikke","den","har","jeg","om","et","men","så","seg","hun","hadde","fra","vi","du","kan","da","ble","ut","skal","vil","ham","etter","over","ved","også","bare","eller","sa","nå","dette","noe","være","meg","mot","opp","der","når","inn","dem","kunne","andre","blir","alle","noen","sin","ha","år","henne","må","selv","sier","få","kom","denne","enn","to","hans","bli","ville","før","vært","skulle","går","her","slik","gikk","mer","hva","igjen","igjen","fikk","man","alt","mange","dash","ingen","dash","ingen","får","oss","under","siden","hele","dag","gang","sammen"]
 
 let setning = "";
+let setningDelt = [];
 let nyttOrd = "";
 let forskyvning = 3;
-let ord = []
+let ord = [];
+let poeng = [];
+let setningPoeng = 0
 
 function definere(){
     setning = document.getElementById("kryptering").value;
@@ -20,10 +23,7 @@ function kryptering() {
         if (splittetSetning[i] == " "){
         splittetSetning[i] = "*"
         console.log(splittetSetning)
-    }
-
-        // console.log(alfabet.indexOf(splittetSetning[i]))
-        
+    }        
         if (alfabet.indexOf(splittetSetning[i]) > (alfabet.length - forskyvning -1)){
             let nyBokstav  = alfabet[alfabet.indexOf(splittetSetning[i]) + forskyvning - alfabet.length]
             nyttOrd = nyttOrd +nyBokstav
@@ -37,22 +37,46 @@ function kryptering() {
 
 
 function dekrypter() {
-    setning = document.getElementById("dekryptering").value
+    
+    definere()
 
     for(let i = 0; i < 27; i++){
+        setning = document.getElementById("dekryptering").value
         forskyvning = i
         kryptering()
         ord.push(nyttOrd)
         console.log(nyttOrd)
         nyttOrd = ""
+        setning = ""
     }
+
+    console.log(poeng)
+    ord = []
+}
+
+function brute(){
+
+    definere()
+
+
+    for(let i=0;i < setningDelt.length; i++){
+            setningDelt = ord[i].split("");
+            if(ordListe.indexOf(setningDelt[i]) > -1){
+               setningPoeng = setningPoeng + 1
+            }
+            poeng.push(setningPoeng)
+            console.log(setningDelt[i])
+        }
 }
 
 function startKryptering(){
     definere()
     kryptering()
     console.log(nyttOrd)
+    nyttOrd = ""
 }
 
-document.getElementById("knapp").addEventListener('click', dekrypter)  
+document.getElementById("knappKryp").addEventListener('click', startKryptering)
+document.getElementById("knappDe").addEventListener('click', dekrypter)  
+document.getElementById("knappBrute").addEventListener('click', brute)  
 
