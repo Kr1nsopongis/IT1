3edb6e2692d16362c8e1252ad2fdb31a1907cf54
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-06-01 22:15:41 +0200

    Ferdistilling

--- siste oppgaver/kode.js
+++ siste oppgaver/kode.js
@@ -1,5 +1,5 @@
-let alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","*"]
-let ordListe = ["og","i","det","på","som","er","en","til","å","han","av","for","med","at","var","de","ikke","den","har","jeg","om","et","men","så","seg","hun","hadde","fra","vi","du","kan","da","ble","ut","skal","vil","ham","etter","over","ved","også","bare","eller","sa","nå","dette","noe","være","meg","mot","opp","der","når","inn","dem","kunne","andre","blir","alle","noen","sin","ha","år","henne","må","selv","sier","få","kom","denne","enn","to","hans","bli","ville","før","vært","skulle","går","her","slik","gikk","mer","hva","igjen","igjen","fikk","man","alt","mange","dash","ingen","dash","ingen","får","oss","under","siden","hele","dag","gang","sammen"]
+let alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","*"];
+let ordListe = ["og","i","det","på","som","er","en","til","å","han","av","for","med","at","var","de","ikke","den","har","jeg","om","et","men","så","seg","hun","hadde","fra","vi","du","kan","da","ble","ut","skal","vil","ham","etter","over","ved","også","bare","eller","sa","nå","dette","noe","være","meg","mot","opp","der","når","inn","dem","kunne","andre","blir","alle","noen","sin","ha","år","henne","må","selv","sier","få","kom","denne","enn","to","hans","bli","ville","før","vært","skulle","går","her","slik","gikk","mer","hva","igjen","igjen","fikk","man","alt","mange","dash","ingen","dash","ingen","får","oss","under","siden","hele","dag","gang","sammen"];
 
 let setning = "";
 let setningDelt = [];
@@ -7,7 +7,9 @@ let nyttOrd = "";
 let forskyvning = 3;
 let ord = [];
 let poeng = [];
-let setningPoeng = 0
+let setningPoeng = 0;
+let maksVerdi = 0;
+let dekrypForslag = ""
 
 function definere(){
     setning = document.getElementById("kryptering").value;
@@ -21,62 +23,91 @@ function kryptering() {
     for(let i = 0; i < splittetSetning.length; i++ ){
 
         if (splittetSetning[i] == " "){
-        splittetSetning[i] = "*"
-        console.log(splittetSetning)
+            splittetSetning[i] = "*"
+            console.log(splittetSetning)
     }        
         if (alfabet.indexOf(splittetSetning[i]) > (alfabet.length - forskyvning -1)){
-            let nyBokstav  = alfabet[alfabet.indexOf(splittetSetning[i]) + forskyvning - alfabet.length]
-            nyttOrd = nyttOrd +nyBokstav
+            var nyBokstav  = alfabet[alfabet.indexOf(splittetSetning[i]) + forskyvning - alfabet.length]
+            nyttOrd += nyBokstav
         } else {
             nyBokstav = alfabet[alfabet.indexOf(splittetSetning[i]) + forskyvning] 
-            nyttOrd = nyttOrd + nyBokstav 
+            nyttOrd += nyBokstav 
             };
         }
-      
 }
 
 
 function dekrypter() {
     
-    definere()
+    definere();
 
     for(let i = 0; i < 27; i++){
         setning = document.getElementById("dekryptering").value
         forskyvning = i
-        kryptering()
-        ord.push(nyttOrd)
-        console.log(nyttOrd)
-        nyttOrd = ""
-        setning = ""
+        kryptering();
+        ord.push(nyttOrd);
+        console.log(nyttOrd);
+        nyttOrd = "";
+        setning = "";
     }
-
-    console.log(poeng)
-    ord = []
+    console.log(ord);
 }
 
 function brute(){
 
-    definere()
+    dekrypter();
 
+    for(let i=0;i < ord.length;i++){
 
-    for(let i=0;i < setningDelt.length; i++){
-            setningDelt = ord[i].split("");
+        setningDelt = ord[i].split("*");
+
+        console.log(setningDelt.length);
+        
+        for(let i=0;i < setningDelt.length; i++){
             if(ordListe.indexOf(setningDelt[i]) > -1){
-               setningPoeng = setningPoeng + 1
+               setningPoeng = setningPoeng + 1;
             }
-            poeng.push(setningPoeng)
             console.log(setningDelt[i])
         }
+        poeng.push(setningPoeng);
+        setningPoeng = 0;
+        
+        console.log(setningDelt);
+        
+    }
+    console.log(poeng)
+
+    for(i = 0;i<poeng.length;i++){
+        if(poeng[i] > maksVerdi){
+            maksVerdi = poeng[i]
+        }
+    }
+
+    console.log(maksVerdi)
+    dekrypForslag = ord[poeng.indexOf(maksVerdi)]
+    console.log(dekrypForslag)
+    document.getElementById("dekryptertOrd").innerHTML = (dekrypForslag)
+
 }
 
 function startKryptering(){
-    definere()
-    kryptering()
-    console.log(nyttOrd)
-    nyttOrd = ""
+    definere();
+    kryptering();
+    console.log(nyttOrd);
+    document.getElementById("kryptertOrd").innerHTML = (nyttOrd);
+    nyttOrd = "";
 }
 
-document.getElementById("knappKryp").addEventListener('click', startKryptering)
-document.getElementById("knappDe").addEventListener('click', dekrypter)  
-document.getElementById("knappBrute").addEventListener('click', brute)  
+function startDekryptering(){
+    document.getElementById("dekryptertOrd").innerHTML = ""
+    dekrypter();
+    
+    for (let i = 0; i < ord.length; i++){
+        document.getElementById("dekryptertOrd").innerHTML += (i+1) + ": " + ord[i] + "<br />";
+    }
+}
+
+document.getElementById("knappKryp").addEventListener('click', startKryptering);
+document.getElementById("knappDe").addEventListener('click', startDekryptering);
+document.getElementById("knappBrute").addEventListener('click', brute);
 
